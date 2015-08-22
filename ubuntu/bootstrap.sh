328af550f58bd06a3b4599317cc627882d450377
Garbin Huang <garbinh@gmail.com>
2015-08-21 21:02:56 -0700

    fixed issue about user exists

--- ubuntu/bootstrap.sh
+++ ubuntu/bootstrap.sh
@@ -2,13 +2,19 @@
 
 set -e
 if [ "$USER" ]; then
-	adduser -q --disabled-password --shell /bin/zsh --gecos '' $USER
-	adduser -q $USER sudo
-	echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
-	zsh -c 'cp -r /root/.zprezto /home/$USER/.zprezto;chown -R $USER:$USER /home/$USER/.zprezto;'
-        su $USER -c 'setopt EXTENDED_GLOB; for rcfile in ${ZDOTDIR:-$HOME}/.zprezto/runcoms/^README.md(.N); do ln -s $rcfile ${ZDOTDIR:-$HOME}/.${rcfile:t}; done;'
-        #su $USER -c 'setopt EXTENDED_GLOB;  for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}";  done;'
-        cd /home/$USER;
+	ret=false
+	getent passwd $USER >/dev/null 2>&1 && ret=true
+        if $ret; then
+                echo "$USER exists"
+        else
+		adduser -q --disabled-password --shell /bin/zsh --gecos '' $USER
+		adduser -q $USER sudo
+		echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+		zsh -c 'cp -r /root/.zprezto /home/$USER/.zprezto;chown -R $USER:$USER /home/$USER/.zprezto;'
+		su $USER -c 'setopt EXTENDED_GLOB; for rcfile in ${ZDOTDIR:-$HOME}/.zprezto/runcoms/^README.md(.N); do ln -s $rcfile ${ZDOTDIR:-$HOME}/.${rcfile:t}; done;'
+		#su $USER -c 'setopt EXTENDED_GLOB;  for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}";  done;'
+        fi
+	cd /home/$USER;
 	su $USER -c "$@"
 else
         exec "$@"
